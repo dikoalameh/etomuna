29e882607d76ea94a253cef894156012aec50011
dikoalameh <bencharlie564@gmail.com>
2025-08-29 19:34:25 +0800

    bagong repo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,8 +8,8 @@
             <div class="flex space-x-2">
                 <div class="w-1/3">
                     <x-text-input id="pi_Lname" class="block mt-1 w-full text-[14px] max-sm:text-[13px] h-[35px]"
-                        type="text" name="pi_Lname" :value="old('pi_Lname')" required autofocus
-                        autocomplete="pi_Lname" placeholder="Last name" />
+                        type="text" name="pi_Lname" :value="old('pi_Lname')" required autofocus autocomplete="pi_Lname"
+                        placeholder="Last name" />
                     <x-input-error :messages="$errors->get('pi_Lname')" class="mt-2" />
                 </div>
                 <div class="w-1/3">
@@ -30,8 +30,8 @@
         <div id="co-wrapper">
             <x-input-label for="Co_Investigators" :value="__('Co-Investigator/s (type N/A if none)')" />
             <x-text-input id="Co_Investigators" class="block mt-1 w-full text-[14px] max-sm:text-[13px] h-[35px]"
-                type="text" name="Co_Investigators" :value="old('Co_Investigators')"
-                autocomplete="Co_Investigators" placeholder="e.g. (John Doe, Carlos González)" />
+                type="text" name="Co_Investigators" :value="old('Co_Investigators')" autocomplete="Co_Investigators"
+                placeholder="e.g. (John Doe, Carlos González)" />
             <x-input-error :messages="$errors->get('Co_Investigators')" class="mt-2" />
         </div>
 
@@ -39,19 +39,28 @@
         <div class="mt-3">
             <label for="checkmcu" class="flex items-center space-x-1">
                 <input id="toggleCheckBox" type="checkbox" class="rounded max-sm:w-[14px] max-sm:h-[14px]"
-                    name="checkmcu" value = "1"/>
-                <span class="text-[14px] text-primary max-sm:text-[13px]">{{ __('Check if not MCU student') }}</span>
+                    name="checkmcu" value="1" />
+                <span id="inputSpan"
+                    class="text-[14px] text-primary max-sm:text-[13px]">{{ __('Check if not MCU student') }}
+                </span>
             </label>
+
+            <x-input-label id="inputLabel" for="pi_program" :value="__('Select Program')" />
+            <x-combo-box id="pi_program" name="pi_program"
+                class="block mt-1 w-full text-[14px] max-sm:text-[13px] h-[35px] leading-[15px]" />
+            <x-input-error :messages="$errors->get('pi_program')" class="mt-2" />
+
             <input type="text" id="textBox"
-                class="h-[35px] text-[14px] block mt-1 w-full border-gray hover:bg-gray rounded-md duration-200"
-                placeholder="School" name="pi_school" disabled />
+                class="h-[35px] text-[14px] block mt-1 w-full border-gray hover:bg-gray rounded-md duration-200 hidden"
+                placeholder="School" name="pi_school" />
         </div>
 
         <!-- Research Title -->
         <div class="mt-2">
             <x-input-label for="pi_title" :value="__('Research Title')" />
             <x-text-input id="pi_title" class="block mt-1 w-full text-[14px] max-sm:text-[13px] h-[35px]" type="text"
-                name="pi_title" :value="old('pi_title')" required autocomplete="pi_title" placeholder="Research Title" />
+                name="pi_title" :value="old('pi_title')" required autocomplete="pi_title"
+                placeholder="Research Title" />
             <x-input-error :messages="$errors->get('pi_title')" class="mt-2" />
         </div>
 
@@ -62,14 +71,6 @@
                 name="pi_Email" :value="old('pi_Email')" required autocomplete="username"
                 placeholder="you@example.com" />
             <x-input-error :messages="$errors->get('pi_Email')" class="mt-2" />
-        </div>
-
-        <!-- Program -->
-        <div class="mt-2">
-            <x-input-label for="pi_program" :value="__('Select Program')" />
-            <x-combo-box id="pi_program" name="pi_program"
-                class="block mt-1 w-full text-[14px] max-sm:text-[13px] h-[35px] leading-[15px]" />
-            <x-input-error :messages="$errors->get('pi_program')" class="mt-2" />
         </div>
 
         <!-- Password -->
@@ -83,9 +84,9 @@
         <!-- Confirm Password -->
         <div class="mt-2">
             <x-input-label for="pi_Password" :value="__('Confirm Password')" />
-            <x-text-input id="pi_Password_confirmation" class="block mt-1 w-full text-[14px] max-sm:text-[13px] h-[35px]"
-                type="password" name="pi_Password_confirmation" required autocomplete="pi_Password"
-                placeholder="Confirm Password" />
+            <x-text-input id="pi_Password_confirmation"
+                class="block mt-1 w-full text-[14px] max-sm:text-[13px] h-[35px]" type="password"
+                name="pi_Password_confirmation" required autocomplete="pi_Password" placeholder="Confirm Password" />
             <x-input-error :messages="$errors->get('pi_Password_confirmation')" class="mt-2" />
         </div>
 
@@ -110,13 +111,29 @@
     </form>
 </x-guest-layout>
 <script>
-    // disable enable textbox
     document.addEventListener('DOMContentLoaded', function () {
-        const checkbox = document.getElementById('toggleCheckBox');
-        const textbox = document.getElementById('textBox');
+        const toggleCheckBox = document.getElementById('toggleCheckBox');
+        const comboBox = document.getElementById('pi_program');
+        const textBox = document.getElementById('textBox');
+        const inputSpan = document.getElementById('inputSpan');
+        const dynamicLabel = document.getElementById('inputLabel');
 
-        checkbox.addEventListener('change', function () {
-            textbox.disabled = !this.checked;
+        toggleCheckBox.addEventListener('change', function () {
+            if (toggleCheckBox.checked) {
+                comboBox.classList.add('hidden');
+                textBox.classList.remove('hidden');
+                textBox.disabled = false;
+
+                // Update the x-input-label's text
+                dynamicLabel.textContent = 'School';
+            } else {
+                comboBox.classList.remove('hidden');
+                textBox.classList.add('hidden');
+                textBox.disabled = true;
+
+                // Restore label text
+                dynamicLabel.textContent = 'Select Program';
+            }
         });
     });
 </script>
